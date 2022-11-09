cd180ab2e1deef0b2b0f89ee5dfcc40ebfe4f68f
midriddler <raekon92@gmail.com>
2022-11-09 14:12:09 +0100

    fixed f-string in tests

--- tests/test_linux.py
+++ tests/test_linux.py
@@ -5,27 +5,26 @@ import _functions
 username = getlogin()
 
 
-@mock.patch("builtins.input", return_value="/home/mindriddler/Skrivbord/test/")
+@mock.patch("builtins.input", return_value=f"/home/{username}/Skrivbord/test/")
 def test_get_file_path(mocked_input):
-    assert _functions.get_file_path() == "/home/mindriddler/Skrivbord/test/"
+    assert _functions.get_file_path() == f"/home/{username}/Skrivbord/test/"
 
 
+@mock.patch("builtins.open", side_effect=[Exception, True])
 @mock.patch("os.remove", return_value=True)
-@mock.patch("builtins.input", return_value="/home/mindriddler/Skrivbord/")
-def test_check_user_write_rights_False_linux(
-    mocked_remove,
-    mocked_input,
-):
+@mock.patch("builtins.input", return_value=f"/home/{username}/Skrivbord/")
+def test_check_user_write_rights_False_linux(mocked_open, mocked_remove,
+                                             mocked_input):
     assert _functions.check_user_write_rights(
-        "/", operating_system="Linux") == "/home/mindriddler/Skrivbord/"
+        "/", operating_system="Linux") == f"/home/{username}/Skrivbord/"
 
 
 @mock.patch("os.remove", return_value=True)
 @mock.patch("builtins.open", return_value=True)
 def test_check_user_write_rights_True_linux(mocked_open, mocked_remove):
     assert _functions.check_user_write_rights(
-        "/home/mindriddler/Skrivbord/",
-        operating_system="Linux") == "/home/mindriddler/Skrivbord/"
+        f"/home/{username}/Skrivbord/",
+        operating_system="Linux") == f"/home/{username}/Skrivbord/"
 
 
 def test_check_backslash_linux():
